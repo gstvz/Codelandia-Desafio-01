391ae49512015438a6f2e22277223c73aa560130
Gustavo França <gstvz.franca@gmail.com>
2022-04-21 16:03:44 -0300

    feat: Created Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header, Main } from "@components";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <Main>{children}</Main>
+    </>
+  );
+};
